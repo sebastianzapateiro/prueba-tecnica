ab69cdf511962e9d740f18a818fe0aee4344ba43
Sebastián Zapateiro <zapateirosebastian@gmail.com>
2022-08-27 23:23:15 -0500

    Realizando maquetado principal

---
+++ src/components/Home.jsx
@@ -0,0 +1,93 @@
+
+import React from 'react'
+import PokemonCard from './PokemonCard'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import icon from '../assets/pikachu.png'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Col, Nav, Row } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+
+function Home() {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
+
+
+        <Container className=''>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+
+              <Nav.Link href="#action1" className='text-white'>Home</Nav.Link>
+              <Nav.Link href="#action1" className='text-white'>Own pokemon list</Nav.Link>
+              <Nav.Link href="#action1" className='text-white'>Create pokemon</Nav.Link>
+              <Nav.Link href="#action1" className='text-white'>Edit pokemon</Nav.Link>
+              <Nav.Link href="#action1" className='text-white'>Delete pokemon</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Nav>
+
+            <NavDropdown className='text-white' title="" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <img
+              alt=""
+              src={icon}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+          </Nav>
+
+
+        </Container>
+      </Navbar>
+
+
+      <Container className='mt-5'>
+
+        <InputGroup className="my-4">
+          <Form.Control aria-label="Text input with dropdown button" />
+
+          <DropdownButton
+            variant="outline-secondary"
+            title="Dropdown"
+            id="input-group-dropdown-2"
+            align="end"
+          >
+            <Dropdown.Item href="#">Action</Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item href="#">Separated link</Dropdown.Item>
+          </DropdownButton>
+        </InputGroup>
+
+        <div className='gap-4 d-flex flex-wrap  justify-content-center'>
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+        </div>
+      </Container>
+
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+function PokemonCard() {
+    return (
+
+
+           
+
+            <Card style={{ width: '16.3rem' }}>
+                <Card.Img variant="top" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+    )
+}
+
+export default PokemonCard
